4fdbcbbdbe6a59defb6bdb78ba834dd37eded8f4
Punit-Rawat12 <punitrrr4@gmail.com>
2026-05-18 21:19:59 +0530

    Added new Problem

---
+++ chapter2_practice_Set/problem4.py
@@ -0,0 +1,7 @@
+# comapring operators to find whather a is greater than b , a,b =34 and 80
+a = 34 
+b = 80 
+if a > b :
+    print("a is greater than b")
+else : 
+    print("b is greater than a ")
