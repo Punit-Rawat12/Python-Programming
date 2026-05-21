43ce1f60e4e4c7f825dc839667989c90dd0ec159
Punit-Rawat12 <punitrrr4@gmail.com>
2026-05-21 22:21:10 +0530

    Add in repo

---
+++ Chpter_4_Practiceset/01_Problem1.py
@@ -0,0 +1,11 @@
+# Program to store fruit names list from user 
+fruit = []
+F1 = input("Enter the name of fruit 1: ")
+fruit.append(F1)
+F2 = input("Enter the name of fruit 2: ")
+fruit.append(F2)
+F3 = input("Enter the name of fruit 3: ")
+fruit.append(F3)
+F4 = input("Enter the name of fruit 4: ")
+fruit.append(F4)    
+print("The list of fruits is: ", fruit)
---
+++ Chpter_4_Practiceset/02_Problem2.py
@@ -0,0 +1,16 @@
+# Program to accept marks of 6 student and display them in a sorted manner
+marks = []
+M1 = int(input("Enter the marks of student 1: "))
+marks.append(M1)
+M2 = int(input("Enter the marks of student 2: "))
+marks.append(M2)
+M3 = int(input("Enter the marks of student 3: "))
+marks.append(M3)
+M4 = int(input("Enter the marks of student 4: "))
+marks.append(M4)
+M5 = int(input("Enter the marks of student 5: "))
+marks.append(M5)
+M6 = int(input("Enter the marks of student 6: "))
+marks.append(M6)
+marks.sort()
+print("The marks of students in sorted manner are: ", marks)
