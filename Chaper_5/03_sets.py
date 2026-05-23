010bb962bbe8673aa67d01c089be27b66223d8f9
Punit-Rawat12 <punitrrr4@gmail.com>
2026-05-23 22:22:15 +0530

    add in a new repo

--- Chaper_5/03_sets.py
+++ Chaper_5/03_sets.py
@@ -1,4 +1,6 @@
 s = {25, "Anant", 55.5, True}
 print(type(s))
 e = set() #e is empty set here 
-print(type(e))
+print(s,type(e))
+s.add("Anant chutiya")
+print(s)
---
+++ Chaper_5/04_setMethods.py
@@ -0,0 +1,4 @@
+s = {25, "Anant", 55.5, True}
+s.add(5)
+s.remove("Anant")
+print(s)
