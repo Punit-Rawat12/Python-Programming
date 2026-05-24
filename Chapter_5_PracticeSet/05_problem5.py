cd25843cf061190a99b623b1042664ed6e1b44a3
Punit-Rawat12 <punitrrr4@gmail.com>
2026-05-24 10:24:19 +0530

    added in new repo

---
+++ Chapter_5_PracticeSet/05_problem5.py
@@ -0,0 +1,17 @@
+d = {}
+name = input("Enter Friend name :")
+lang = input("Enter your fav language :")
+d.update({name : lang})
+
+name = input("Enter Friend name :")
+lang = input("Enter your fav language :")
+d.update({name : lang})
+
+name = input("Enter Friend name :")
+lang = input("Enter your fav language :")
+d.update({name : lang})
+
+name = input("Enter Friend name :")
+lang = input("Enter your fav language :")
+d.update({name : lang})
+print(d)
