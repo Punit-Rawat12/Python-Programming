62c45b9c6fa73089b53513f44486b2b846e8f4e7
Punit-Rawat12 <punitrrr4@gmail.com>
2026-05-24 11:30:21 +0530

    updated  02_conditions.py

--- Chapter_6/01_conditions.py
+++ Chapter_6/01_conditions.py
@@ -1,6 +1,6 @@
 age = int(input("Enter you age:"))
-if(age>18):
-    print("You are above the age of concent")
+if(age>= 18):
+    print("You are above the age of consent")
 
 else: 
-    print("You are below the age of concent")
+    print("You are below the age of consent")
