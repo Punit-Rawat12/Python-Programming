75c0b181e85ee50a9c0af3e4fbcd311a390c52ae
Punit-Rawat12 <punitrrr4@gmail.com>
2026-05-25 23:51:41 +0530

    added in repo

--- Chapter_6_PrcticeSet/01_Problem.py
+++ Chapter_6_PrcticeSet/01_Problem.py
@@ -1,15 +1,17 @@
-no = int(input("Enter a 4 digit number"))
-no1 = int(input("Enter a 4 digit number"))
-no2 = int(input("Enter a 4 digit number"))
-no3= int(input("Enter a 4 digit number"))
-if (no > no1 and no > no2 and no > no3):
+no = int(input("Enter a 4 digit number :"))
+no1 = int(input("Enter a 4 digit number :"))
+no2 = int(input("Enter a 4 digit number :"))
+no3= int(input("Enter a 4 digit number :"))
+if (no >= no1 and no > no2 and no > no3):
     print(no, "is greater 4 digit number")
 
-elif (no1 > no and no1 > no2 and no1 > no3):
+elif (no1 >= no and no1 > no2 and no1 > no3):
     print(no1, "is greater 4 digit number")
 
-elif (no2 > no1 and no2 > no and no2 > no3):
+elif (no2 >= no1 and no2 > no and no2 > no3):
     print(no2, "is greater 4 digit number")
 
 else:
     print(no3, "is greater 4 digit number")
+
+
