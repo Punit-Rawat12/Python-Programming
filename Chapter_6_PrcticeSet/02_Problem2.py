93f7a201600773750a508b15ba6b553be888df3a
Punit-Rawat12 <punitrrr4@gmail.com>
2026-05-26 08:22:38 +0530

    added in new repo

---
+++ Chapter_6_PrcticeSet/02_Problem2.py
@@ -0,0 +1,9 @@
+marks1 = int(input("Enter Yout marks ;"))
+marks2 = int(input("Enter Yout marks ;"))
+marks3 = int(input("Enter Yout marks ;"))
+total_percentage = (marks1 + marks2 + marks3)/300 * 10050
+if(total_percentage>40) :
+    print("You are pass")
+
+else:
+    print("You are not pass")    
