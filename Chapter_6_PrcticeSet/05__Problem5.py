4a903beb8eac94169340ea6393b82c6350934d9e
Punit-Rawat12 <punitrrr4@gmail.com>
2026-05-26 11:35:55 +0530

    Added in new repo

---
+++ Chapter_6_PrcticeSet/05__Problem5.py
@@ -0,0 +1,6 @@
+names = ["Anant" , "Nikunj", "Anushka","Punit"]
+name = input("Enter your name ").capitalize()
+if(name in name):
+    print("Your name in list")
+else:
+    print("Your name is not in list ")    
