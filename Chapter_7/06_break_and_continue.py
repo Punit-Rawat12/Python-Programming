424b8d6ebad5c6cf1d0541fa87a98a6c09f6818e
Punit-Rawat12 <punitrrr4@gmail.com>
2026-06-05 10:59:59 +0530

    Practice using break and continue keyword

---
+++ Chapter_7/06_break_and_continue.py
@@ -0,0 +1,11 @@
+# break Keybword
+for i in range(100):
+    if(i == 34):
+        break
+    print(i)
+# continue keyword
+for i in range(0,100):
+    if(i == 34):
+        continue
+    print(i)
+   
