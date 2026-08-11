a71a9db606be42142170588b295d3cebc09d22c3
Punit-Rawat12 <punitrrr4@gmail.com>
2026-08-11 20:49:39 +0530

    Added in repo

---
+++ Chapter9/01_file.py
@@ -0,0 +1,7 @@
+'''
+Atul will be  Founder of Pokhari FC
+'''
+f = open("file.txt")
+data = f.read()
+print(data)
+f.close()
---
+++ Chapter9/02_file_write.py
@@ -0,0 +1,4 @@
+st = "Hey Punit , How u doing these days "
+p = open("myfile.txt" ,"w")
+p.write(st)
+p.close()
