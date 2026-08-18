c4846c7fd54ce474f2fd8108aa7245df9bfa5dc2
Punit-Rawat12 <punitrrr4@gmail.com>
2026-08-18 12:03:43 +0530

    04 file uploaded to git

---
+++ Chapter10/04_staticmethod.py
@@ -0,0 +1,13 @@
+class Marks :
+    Nikunj = 54 
+    Anantt = "jai ho"
+    Punit = 100
+    Atul = 100
+    def Ling(self):
+        print("hey I am the best assasin ut there")
+
+
+Mark = Marks()
+Marks.Anantt = "fd"
+print(Marks.Anantt , Marks.Punit )
+Mark.Ling()
