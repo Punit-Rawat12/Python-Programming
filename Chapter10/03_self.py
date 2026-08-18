fb6560d965ae856bc459e58f8abfc369e9969d47
Punit-Rawat12 <punitrrr4@gmail.com>
2026-08-18 11:53:03 +0530

    self.py updeated

--- Chapter10/03_self.py
+++ Chapter10/03_self.py
@@ -1,8 +1,13 @@
 class Employee :
-    language = "Py"            '''this is classs attribute'''
+    language = "Py"            #'''this is classs attribute'''
     salary = 1200000
+    def getinfo(self):
+        print(f"Print language is {self.language} .The salary is {self.salary}")
+
+
+
 First = Employee()
-Name = "Nikunj"              ''' This is instance  attribute'''
+Name = "Nikunj"              #''' This is instance  attribute'''
 print(First.salary ,Name )
 
 second = Employee()
