3239401c2b2b292d5cb0fe62b5441272ead97a59
Punit-Rawat12 <punitrrr4@gmail.com>
2026-08-19 20:31:13 +0530

    file updated

--- Chapter10/05_constructor.py
+++ Chapter10/05_constructor.py
@@ -1,6 +1,9 @@
 class Employee :
     language = "Py"            #'''this is classs attribute'''
     salary = 1200000
+    def __init__(self):
+        print("I am creating Object ")   #Dunder method fuction who can run without givng call 
+
     def getinfo(self):
         print(f"Print language is {self.language} .The salary is {self.salary}")
 
